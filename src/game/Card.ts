1805e92ec434fb6e720f8a64dab803f76734353b
Vyraal1 <kevin.afable@gmail.com>
2020-07-02 19:39:44 -0400

    feat: card interface

--- src/game/Card.ts
+++ src/game/Card.ts
@@ -1,4 +1,7 @@
-export interface ICard {
+import Game from './Game';
+import Player from './Player';
+
+export default interface Card {
   name: string;
   cost: number;
   play: (game: Game, player: Player) => Game | undefined;
