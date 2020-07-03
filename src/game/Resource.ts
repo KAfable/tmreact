d8b2a0b3f13368e5e6c1998fa24276edd587d071
Vyraal1 <kevin.afable@gmail.com>
2020-07-03 08:48:28 -0400

    feat: resource enums

---
+++ src/game/Resource.ts
@@ -0,0 +1,15 @@
+export enum BasicResource {
+  MEGACREDITS = 'megacredits',
+  STEEL = 'steel',
+  TITANIUM = 'titanium',
+  PLANTS = 'plants',
+  ENERGY = 'energy',
+  HEAT = 'heat',
+}
+
+export enum CardResource {
+  ANIMAL,
+  FIGHTER,
+  FLOATER,
+  MICROBE,
+}
