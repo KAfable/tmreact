8cb1598dbcc3b3ab74291141593dcf08fe1a6c64
Vyraal1 <kevin.afable@gmail.com>
2020-09-02 13:47:09 -0400

    feat: new getter for param to support enums

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -1,19 +1,25 @@
 import Player from './Player';
-// import Board from './board';
+import Board from './board';
 import Card from './card';
 
 const MAX_OXYGEN = 14;
 const MAX_TEMPERATURE = 8;
-// const MAX_OCEANS = 9;
+const MAX_OCEANS = 9;
 const STARTING_TEMP = -30;
 const STARTING_OXYGEN = 0;
+const STARTING_OCEAN = 0;
+const STARTING_VENUS = 0;
 
 export default class Game {
   public players: Array<Player> = [];
   public deck: Array<Card> = [];
   public discard: Array<Card> = [];
-  private oxygen: number = STARTING_OXYGEN;
-  private temperature: number = STARTING_TEMP;
+  private parameters = {
+    [GlobalParameter.OXYGEN]: STARTING_OXYGEN,
+    [GlobalParameter.TEMP]: STARTING_TEMP,
+    [GlobalParameter.OCEAN_COUNT]: STARTING_OCEAN,
+    [GlobalParameter.VENUS]: STARTING_VENUS,
+  };
 
   // needs a board
   // needs milestones
@@ -23,19 +29,27 @@ export default class Game {
     this.players = players;
   }
 
+  public increaseOceanCount() {
+    if (this.parameters[GlobalParameter.OCEAN_COUNT] < MAX_OCEANS)
+      this.parameters[GlobalParameter.OCEAN_COUNT]++;
+  }
   public increaseOxygen() {
-    if (this.oxygen < MAX_OXYGEN) this.oxygen++;
+    if (this.parameters[GlobalParameter.OXYGEN] < MAX_OXYGEN)
+      this.parameters[GlobalParameter.OXYGEN]++;
   }
-
-  public getOxygen() {
-    return this.oxygen;
-  }
-
   public increaseTemperature() {
-    if (this.temperature < MAX_TEMPERATURE) this.oxygen += 2;
+    if (this.parameters[GlobalParameter.TEMP] < MAX_TEMPERATURE)
+      this.parameters[GlobalParameter.TEMP] += 2;
   }
 
-  public getTemperature() {
-    return this.temperature;
+  getParameter(param: GlobalParameter) {
+    return this.parameters[param];
   }
+}
+
+export enum GlobalParameter {
+  OXYGEN = 'oxygen',
+  TEMP = 'temperature',
+  OCEAN_COUNT = 'oceanCount',
+  VENUS = 'venus',
 }
