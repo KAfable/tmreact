09d7cc7db6fcbed39e60c0838591590b8a67831a
Vyraal1 <kevin.afable@gmail.com>
2020-09-02 00:02:42 -0400

    feat: early card class prototype

--- src/game/card/index.ts
+++ src/game/card/index.ts
@@ -1,1 +1,19 @@
-export default class Card {}
+import CardBuilder from '../cardBuilder';
+import { BasicResource } from '../Resource';
+import { Tags } from './Tags';
+import Game from '../Game';
+import Player from '../Player';
+
+export default class Card {
+  public name: string = '';
+  public resources: Array<BasicResource> = [];
+  public cost: number = 0;
+  public tags: Array<Tags> = [];
+  public victoryPoints?: number;
+
+  constructor(cardBuilder: CardBuilder) {
+    this.name = name;
+  }
+
+  play(game: Game, player: Player) {}
+}
