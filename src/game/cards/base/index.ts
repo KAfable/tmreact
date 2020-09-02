cb4cf4eb235c24f11ab91ccb9dbced4c748d5819
Vyraal1 <kevin.afable@gmail.com>
2020-09-02 00:03:57 -0400

    feat: card001 implemented

--- src/game/cards/base/index.ts
+++ src/game/cards/base/index.ts
@@ -3,10 +3,17 @@
 // import Player from '../Player';
 // import { BasicResource } from '../Resource';
 import CardBuilder from '../../cardBuilder';
+import { CardNames } from '../CardNames';
+import { Tags } from 'src/game/card/Tags';
+import { GlobalParameter } from 'src/game/board';
 
 // 001 Colonizer Training Camp
-const colonizerTrainingcamp = CardBuilder('Colonizer Training Camp');
-
-//
+const colonizerTrainingcamp = new CardBuilder(CardNames.COLONIZER_TRAINING_CAMP)
+  .setID('001')
+  .isProject(8)
+  .addTags([Tags.JOVIAN, Tags.BUILDING])
+  .addGlobalRequirement({ min: true, amount: 5, type: GlobalParameter.OXYGEN })
+  .addVictoryPoints(2)
+  .build();
 
 module.exports = { colonizerTrainingcamp };
