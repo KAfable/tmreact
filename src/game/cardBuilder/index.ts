7bb46dec204af6c44e658b3f9d0c1b71ea2aa726
Vyraal1 <kevin.afable@gmail.com>
2020-09-02 00:03:41 -0400

    feat: cardBuilder prototype

--- src/game/cardBuilder/index.ts
+++ src/game/cardBuilder/index.ts
@@ -1,5 +1,75 @@
 import Card from '../card';
+import { CardNames } from '../cards/CardNames';
+import { Tags } from '../card/Tags';
+import { GlobalParameter } from '../board';
 
-export default function CardBuilder(name: String): Card {
-  return new Card();
+interface GlobalRequirement {
+  min: boolean;
+  amount: number;
+  type: GlobalParameter;
+}
+
+export default class CardBuilder {
+  private _id!: string;
+  private _name: string;
+  private _cost!: number;
+  private _tags: Array<Tags> = [];
+  private _globalReq?: GlobalRequirement;
+  private _victoryPoints?: number;
+
+  constructor(name: CardNames) {
+    this._name = name;
+    return this;
+  }
+
+  addGlobalRequirement(req: GlobalRequirement) {
+    this._globalReq = req;
+    return this;
+  }
+
+  addTags(tags: Tags | Array<Tags>): CardBuilder {
+    if (Array.isArray(tags)) {
+      this._tags = [...this._tags, ...tags];
+    } else {
+      this._tags = [...this._tags, tags];
+    }
+    return this;
+  }
+
+  addVictoryPoints(points: number) {
+    this._victoryPoints = points;
+    return this;
+  }
+
+  build(): Card {
+    return new Card(this);
+  }
+
+  isProject(cost: number): CardBuilder {
+    return this;
+  }
+
+  setID(input: string) {
+    this._id = input;
+    return this;
+  }
+
+  get id() {
+    return this._id;
+  }
+  get name() {
+    return this._name;
+  }
+  get cost() {
+    return this._cost;
+  }
+  get tags() {
+    return this._tags;
+  }
+  get globalReq() {
+    return this._globalReq;
+  }
+  get victoryPoints() {
+    return this._victoryPoints;
+  }
 }
